4f121e07ea02cea430d4b44939b291f58272ec5b
rafael <messiasrafaelb@gmail.com>
2025-08-31 21:41:12 -0300

    feat(repository): add UserRepository interface

---
+++ src/main/java/br/com/mrb/application/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.mrb.application.repository;
+
+import br.com.mrb.application.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
