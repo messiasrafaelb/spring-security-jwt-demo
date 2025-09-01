d8033612cd6db99b9cb7f706b9bd4aa22c8d5c62
rafael <messiasrafaelb@gmail.com>
2025-08-31 21:27:11 -0300

    feat(model): add Lombok annotations

--- src/main/java/br/com/mrb/application/model/Role.java
+++ src/main/java/br/com/mrb/application/model/Role.java
@@ -1,14 +1,20 @@
 package br.com.mrb.application.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "roles")
+@Setter @Getter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Role {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -24,5 +30,10 @@ public class Role {
             mappedBy = "role",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
-    private Set<UserRole> userRoles = new HashSet<>();
+    private final Set<UserRole> userRoles = new HashSet<>();
+
+    public Role(RoleName name) {
+        this.name = name;
+    }
 }
+
--- src/main/java/br/com/mrb/application/model/User.java
+++ src/main/java/br/com/mrb/application/model/User.java
@@ -1,21 +1,30 @@
 package br.com.mrb.application.model;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Setter @Getter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = false)
     private String name;
 
-    @Column(nullable = false, unique = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false, unique = false)
@@ -25,5 +34,11 @@ public class User {
             mappedBy = "user",
             cascade = CascadeType.ALL,
             orphanRemoval = true)
-    private Set<UserRole> userRoles = new HashSet<>();
+    private final Set<UserRole> userRoles = new HashSet<>();
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
