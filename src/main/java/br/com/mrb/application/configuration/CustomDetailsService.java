d40ea3c852dc2ada973a8fda3208018a4703ee22
rafael <messiasrafaelb@gmail.com>
2025-09-13 22:55:48 -0300

    feat(configuration): add CustomDetailsService class to load a user

---
+++ src/main/java/br/com/mrb/application/configuration/CustomDetailsService.java
@@ -0,0 +1,26 @@
+package br.com.mrb.application.configuration;
+
+import br.com.mrb.application.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CustomDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var authorities = user.getUserRoles().stream().map(userRole -> new SimpleGrantedAuthority(userRole.getRole().getName().name())).toList();
+        return new User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
