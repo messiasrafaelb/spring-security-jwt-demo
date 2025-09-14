e30abd81474225945222ae229c4ab7436c022e60
rafael <messiasrafaelb@gmail.com>
2025-09-13 21:24:48 -0300

    feat(repository): add @Query in findByEmail method to eagerly fetch Roles

--- src/main/java/br/com/mrb/application/repository/UserRepository.java
+++ src/main/java/br/com/mrb/application/repository/UserRepository.java
@@ -2,11 +2,18 @@ package br.com.mrb.application.repository;
 
 import br.com.mrb.application.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("""
+    select u from User u
+    left join fetch u.userRoles ur
+    left join fetch ur.role r
+    where u.email = :email
+    """)
     Optional<User> findByEmail(String email);
 }
