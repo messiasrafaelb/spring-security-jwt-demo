f4e43ea2cd183f64ad56bd72fe3b84f58e0f3fdd
rafael <messiasrafaelb@gmail.com>
2025-09-13 23:45:14 -0300

    feat(controller): add AuthController class

---
+++ src/main/java/br/com/mrb/application/controller/AuthController.java
@@ -0,0 +1,37 @@
+package br.com.mrb.application.controller;
+
+import br.com.mrb.application.dto.AuthRequest;
+import br.com.mrb.application.dto.AuthResponse;
+import br.com.mrb.application.service.CustomDetailsService;
+import br.com.mrb.application.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationManager authManager;
+    private final CustomDetailsService userDetailsService;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        var authentication = authManager.authenticate(
+                new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(
+                        request.email(), request.password()
+                )
+        );
+
+        var user = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.generateToken(user);
+        return ResponseEntity.ok(new AuthResponse(token));
+    }
+}
