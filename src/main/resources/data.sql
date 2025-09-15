d2eeab00bbb9875d14d845cd6ad0264fbc0805f1
rafael <messiasrafaelb@gmail.com>
2025-09-15 00:16:25 -0300

    chore(resources): adjust inserts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,9 @@ INSERT INTO roles (id, name) VALUES (1, 'ROLE_ADMIN');
 INSERT INTO roles (id, name) VALUES (2, 'ROLE_USER');
 
 -- USERS (use os hashes gerados no console)
-INSERT INTO users (id, name, email, password) VALUES
-  (1, 'Alice Admin', 'alice@ex.com', '{bcrypt}$2a$10$UrUbd9LVENfLG5gBqIQGK.kyJyPeT1cFroVleG5ZxCB.zLz5sGFY2'),
-  (2, 'Bob User',   'bob@ex.com',   '{bcrypt}$2a$10$K2rYy3dmZlDKM.9ualp8XuTnhNn2sizCGZ02.HPH.Rd0mAUgI4tka');
+INSERT INTO users (name, email, password) VALUES
+  ('Alice Admin', 'alice@ex.com', '{bcrypt}$2a$10$UrUbd9LVENfLG5gBqIQGK.kyJyPeT1cFroVleG5ZxCB.zLz5sGFY2'),
+  ('Bob User',   'bob@ex.com',   '{bcrypt}$2a$10$K2rYy3dmZlDKM.9ualp8XuTnhNn2sizCGZ02.HPH.Rd0mAUgI4tka');
 
 -- USERS_ROLES (id é identity; pode omitir)
 INSERT INTO users_roles (user_id, role_id) VALUES (1, 1); -- Alice ADMIN admin123
