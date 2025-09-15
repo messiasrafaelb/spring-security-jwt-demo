373e7fb3e672bfa50a644be246ab3981ad873ae0
rafael <messiasrafaelb@gmail.com>
2025-09-15 00:00:18 -0300

    feat(services): add RegisterService class

---
+++ src/main/java/br/com/mrb/application/service/RegisterService.java
@@ -0,0 +1,37 @@
+package br.com.mrb.application.service;
+
+import br.com.mrb.application.dto.RegisterRequest;
+import br.com.mrb.application.dto.RegisterResponse;
+import br.com.mrb.application.exception.EmailAlreadyExistsException;
+import br.com.mrb.application.exception.RoleNotFoundException;
+import br.com.mrb.application.mapper.UserMapper;
+import br.com.mrb.application.model.Role;
+import br.com.mrb.application.repository.RoleRepository;
+import br.com.mrb.application.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static br.com.mrb.application.model.Role.RoleName;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@Service
+@RequiredArgsConstructor
+public class RegisterService {
+
+    private final UserRepository user_repository;
+    private final RoleRepository role_repository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper mapper;
+
+    public RegisterResponse save(RegisterRequest request) {
+        if (user_repository.findByEmail(request.getEmail()).isPresent())
+            throw new EmailAlreadyExistsException(BAD_REQUEST, "Email already exists.");
+        Role role = role_repository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RoleNotFoundException(NOT_FOUND, "Role not found"));
+        var user = mapper.toEntity(request, role, passwordEncoder);
+        user_repository.save(user);
+        return mapper.toResponse(user);
+    }
+}
