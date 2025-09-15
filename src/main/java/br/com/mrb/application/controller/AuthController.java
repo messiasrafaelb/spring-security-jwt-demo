e7a9ef74895cc1eb2fc076821960aa0fb8a62005
rafael <messiasrafaelb@gmail.com>
2025-09-15 00:12:19 -0300

    feat(controller): implement register post method

--- src/main/java/br/com/mrb/application/controller/AuthController.java
+++ src/main/java/br/com/mrb/application/controller/AuthController.java
@@ -2,8 +2,12 @@ package br.com.mrb.application.controller;
 
 import br.com.mrb.application.dto.AuthRequest;
 import br.com.mrb.application.dto.AuthResponse;
+import br.com.mrb.application.dto.RegisterRequest;
+import br.com.mrb.application.dto.RegisterResponse;
 import br.com.mrb.application.service.CustomDetailsService;
 import br.com.mrb.application.service.JwtService;
+import br.com.mrb.application.service.RegisterService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,19 +25,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthenticationManager authManager;
-    private final CustomDetailsService userDetailsService;
     private final JwtService jwtService;
+    private final RegisterService registerService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        var authentication = authManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        request.email(), request.password()
-                )
-        );
-
+        var authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(
+                                                            request.email(), request.password()));
         var user = (UserDetails) authentication.getPrincipal();
         String token = jwtService.generateToken(user);
         return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest request) {
+        var user = registerService.save(request);
+        return ResponseEntity.status(CREATED).body(user);
     }
 }
