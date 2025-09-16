922593cfdaad05aa002351b75b30d53fa78b1e5b
rafael <messiasrafaelb@gmail.com>
2025-09-16 11:28:38 -0300

    feat(dto): add PostResponse class

---
+++ src/main/java/br/com/mrb/application/dto/PostResponse.java
@@ -0,0 +1,7 @@
+package br.com.mrb.application.dto;
+
+import java.time.LocalDateTime;
+
+public record PostResponse(Long id, String title, String content,
+                           LocalDateTime createdAt, LocalDateTime updatedAt, String authorName) {
+}
