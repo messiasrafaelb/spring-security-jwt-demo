1a3594a064e133090dbe524dcc74c3623c808116
rafael <messiasrafaelb@gmail.com>
2025-09-16 08:45:49 -0300

    feat(application): add @EnableJpaAuditing

--- src/main/java/br/com/mrb/application/SpringSecurityApplication.java
+++ src/main/java/br/com/mrb/application/SpringSecurityApplication.java
@@ -2,8 +2,10 @@ package br.com.mrb.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
