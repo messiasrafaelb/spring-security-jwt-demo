b124ecabf95e514db7c6b4580ed0b68d13b145b8
rafael <messiasrafaelb@gmail.com>
2025-09-18 13:13:44 -0300

    refactor(exception): adjust constructor

--- src/main/java/br/com/mrb/application/exception/RoleNotFoundException.java
+++ src/main/java/br/com/mrb/application/exception/RoleNotFoundException.java
@@ -1,9 +1,7 @@
 package br.com.mrb.application.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class RoleNotFoundException extends RuntimeException {
-    public RoleNotFoundException(HttpStatus badRequest, String message) {
+    public RoleNotFoundException(String message) {
         super(message);
     }
 }
