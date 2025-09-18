f295895454e97fb18f54ddd079620c7ca4a15bab
rafael <messiasrafaelb@gmail.com>
2025-09-18 13:23:44 -0300

    feat(configuration/security): add AuditConfig

---
+++ src/main/java/br/com/mrb/application/configuration/security/AuditConfig.java
@@ -0,0 +1,21 @@
+package br.com.mrb.application.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.context.SecurityContextHolder;
+import java.security.Principal;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return () -> Optional.ofNullable(
+                SecurityContextHolder.getContext().getAuthentication()
+        ).map(Principal::getName);
+    }
+}
