768094ca9eea916b8d4271c87d44ac87301d1593
rafael <messiasrafaelb@gmail.com>
2025-09-18 14:58:00 -0300

    refactor(controller): adjust GetMapping response

--- src/main/java/br/com/mrb/application/controller/RootController.java
+++ src/main/java/br/com/mrb/application/controller/RootController.java
@@ -3,8 +3,10 @@ package br.com.mrb.application.controller;
 import br.com.mrb.application.assembler.RootModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,7 +15,7 @@ public class RootController {
     private final RootModel assembler;
 
     @GetMapping("/")
-    public RepresentationModel<?> root() {
-        return assembler.toModel();
+    public ResponseEntity<RepresentationModel<?>> root() {
+        return ResponseEntity.status(OK).body(assembler.toModel());
     }
 }
