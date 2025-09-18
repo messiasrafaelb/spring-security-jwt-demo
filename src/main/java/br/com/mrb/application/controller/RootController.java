8cca9da1091f8006305db8cf1a9173b12cd8d03f
rafael <messiasrafaelb@gmail.com>
2025-09-18 14:55:30 -0300

    refactor(controller): extract HATEOAS assembler to RootModel class

--- src/main/java/br/com/mrb/application/controller/RootController.java
+++ src/main/java/br/com/mrb/application/controller/RootController.java
@@ -1,22 +1,19 @@
 package br.com.mrb.application.controller;
 
+import br.com.mrb.application.assembler.RootModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.RepresentationModel;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import static org.springframework.http.HttpStatus.*;
 
 @RestController
+@RequiredArgsConstructor
 public class RootController {
 
+    private final RootModel assembler;
+
     @GetMapping("/")
-    public ResponseEntity<RepresentationModel<?>> root() {
-        var model = new RepresentationModel<>();
-        model.add(linkTo(methodOn(AuthController.class).login(null)).withRel("login"));
-        model.add(linkTo(methodOn(PostController.class).getAllPosts(null)).withRel("posts"));
-        model.add(linkTo(methodOn(UserAdminController.class).getAllUsers(null)).withRel("users"));
-        return ResponseEntity.status(OK).body(model);
+    public RepresentationModel<?> root() {
+        return assembler.toModel();
     }
 }
