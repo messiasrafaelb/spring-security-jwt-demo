e71dd38c635a7a04b9a395fd16e3184c0329b843
rafael <messiasrafaelb@gmail.com>
2025-09-19 17:29:21 -0300

    refactor(assembler): refator userAdminModelAssembler class

--- src/main/java/br/com/mrb/application/assembler/UserAdminModelAssembler.java
+++ src/main/java/br/com/mrb/application/assembler/UserAdminModelAssembler.java
@@ -4,14 +4,16 @@ import br.com.mrb.application.controller.UserAdminController;
 import br.com.mrb.application.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Component;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import static br.com.mrb.application.model.Role.RoleName.ROLE_ADMIN;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
@@ -21,23 +23,25 @@ public class UserAdminModelAssembler {
 
     private final PagedResourcesAssembler<UserResponse> pagedResourcesAssembler;
 
-    public EntityModel<UserResponse> addPatchLink(UserResponse user) {
-        var page = PageRequest.of(0, 15, Sort.by("name").ascending());
-        return EntityModel.of(user,
-                linkTo(methodOn(UserAdminController.class).getAllUsers(page)).withRel("users"),
-                linkTo(methodOn(UserAdminController.class).deleteUser(user.id())).withRel("delete"));
+    private List<Link> baseLinks(UserResponse user, Pageable pageable) {
+        var links = new ArrayList<Link>();
+        links.add(linkTo(methodOn(UserAdminController.class).getAllUsers(pageable)).withRel("users"));
+        links.add(linkTo(methodOn(UserAdminController.class).deleteUser(user.id())).withRel("delete"));
+        if (!user.roleSet().contains(ROLE_ADMIN))
+            links.add(linkTo(methodOn(UserAdminController.class).changeUserRole(user.id(), ROLE_ADMIN.name())).withRel("make-admin"));
+        return links;
     }
 
-    public EntityModel<Map<String, String>> addDeleteLink(Map<String, String> map) {
-        var page = PageRequest.of(0, 15, Sort.by("name").ascending());
-        return EntityModel.of(map, linkTo(methodOn(UserAdminController.class).getAllUsers(page)).withRel("users"));
+    public EntityModel<UserResponse> toModel(UserResponse user, Pageable pageable) {
+        return EntityModel.of(user, baseLinks(user, pageable));
     }
 
-    public PagedModel<EntityModel<UserResponse>> addGetAllLink(Page<UserResponse> page) {
-        return pagedResourcesAssembler.toModel(page, user -> EntityModel.of(user,
-                linkTo(methodOn(UserAdminController.class).changeUserRole(user.id(), "ADMIN")).withRel("make-admin"),
-                linkTo(methodOn(UserAdminController.class).changeUserRole(user.id(), "USER")).withRel("make-user"),
-                linkTo(methodOn(UserAdminController.class).deleteUser(user.id())).withRel("delete")));
+    public PagedModel<EntityModel<UserResponse>> toPagedModel(Page<UserResponse> page,Pageable pageable) {
+        return pagedResourcesAssembler.toModel(page, user -> toModel(user, pageable));
+    }
+
+    public EntityModel<Map<String, String>> deletedResponse(Map<String, String> map, Pageable pageable) {
+        return EntityModel.of(map, linkTo(methodOn(UserAdminController.class).getAllUsers(pageable)).withRel("users"));
     }
 
 }
