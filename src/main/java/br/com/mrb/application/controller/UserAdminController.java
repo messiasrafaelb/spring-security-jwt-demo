84e50426081c5d1254f16f0e1482c6134b201c1f
rafael <messiasrafaelb@gmail.com>
2025-09-19 17:30:05 -0300

    refactor(controller): adjust UserAdminController class

--- src/main/java/br/com/mrb/application/controller/UserAdminController.java
+++ src/main/java/br/com/mrb/application/controller/UserAdminController.java
@@ -6,6 +6,7 @@ import br.com.mrb.application.service.UserAdminService;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.hateoas.EntityModel;
@@ -14,8 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 import java.util.Map;
 import static org.springframework.data.domain.Sort.Direction.ASC;
 import static org.springframework.http.HttpStatus.OK;
@@ -34,23 +33,27 @@ public class UserAdminController {
     public ResponseEntity<PagedModel<EntityModel<UserResponse>>> getAllUsers(
             @PageableDefault(page = 0, size = 15, sort = "name", direction = ASC) Pageable pageable) {
         Page<UserResponse> page = userAdminService.findAll(pageable);
-        var pagedModel = assembler.addGetAllLink(page);
+        var pagedModel = assembler.toPagedModel(page, pageable);
         return ResponseEntity.status(OK).body(pagedModel);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<EntityModel<Map<String,String>>> deleteUser(
-            @PathVariable @Positive(message = "Id must be greater than 0") Long id) {
-        Map<String, String> message = userAdminService.deleteById(id);
-        return ResponseEntity.status(OK).body(assembler.addDeleteLink(message));
     }
 
     @PatchMapping("/{id}/role")
     public ResponseEntity<EntityModel<UserResponse>> changeUserRole(
             @PathVariable @Positive(message = "Id must be greater than 0") Long id,
             @RequestParam String role) {
+        var pageable = PageRequest.of(0, 15, ASC);
         var user = userAdminService.changeRole(id, role);
-        return ResponseEntity.status(OK).body(assembler.addPatchLink(user));
+        var model = assembler.toModel(user, pageable);
+        return ResponseEntity.status(OK).body(model);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EntityModel<Map<String,String>>> deleteUser(
+            @PathVariable @Positive(message = "Id must be greater than 0") Long id) {
+        var pageable = PageRequest.of(0, 15, ASC);
+        var map = userAdminService.deleteById(id);
+        var model = assembler.deletedResponse(map, pageable);
+        return ResponseEntity.status(OK).body(model);
     }
 
 }
